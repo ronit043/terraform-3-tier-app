58361540c910f013d042b84f8ec38d703420df11
ronit043 <smodak2000@gmail.com>
2023-03-15 09:53:05 +0530

    first commit

---
+++ servers.tf
@@ -0,0 +1,51 @@
+#Author     : Soumya Modak
+#LinkedIn   : www.linkedin.com/in/soumya-modak
+#GitHub     : https://github.com/ronit043
+########################################################
+
+resource "aws_instance" "web" {
+  ami                         = var.ec2_ami
+  instance_type               = var.instance_type
+  key_name                    = "ronit043"
+  subnet_id                   = aws_subnet.public[count.index].id
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+  associate_public_ip_address = true
+  count                       = 2
+
+  user_data = <<-EOF
+        #!/bin/bash
+          sudo yum update -y
+          sudo yum install -y httpd.x86_64
+          sudo systemctl start httpd.service
+          sudo systemctl enable httpd.service
+          echo “Welcome and Learn with Soumya from $(hostname -f)” > /var/www/html/index.html
+        EOF
+
+  tags = {
+    Name = "Web_Server_${count.index}"
+  }
+
+  provisioner "file" {
+    source      = "./ronit043.pem"
+    destination = "/home/ec2-user/ronit043.pem"
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("/home/ronit/.ssh/ronit043.pem")
+    }
+  }
+}
+
+resource "aws_instance" "db" {
+  ami                    = var.ec2_ami
+  instance_type          = var.instance_type
+  key_name               = "ronit043"
+  subnet_id              = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.allow_tls_db.id]
+
+  tags = {
+    Name = "Database_server"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+#Author     : Soumya Modak
+#LinkedIn   : www.linkedin.com/in/soumya-modak
+#GitHub     : https://github.com/ronit043
+########################################################
+
+variable "cidr" {
+  type    = list(any)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "az" {
+  type    = list(any)
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ec2_ami" {
+  type    = string
+  default = "ami-005f9685cb30f234b"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
